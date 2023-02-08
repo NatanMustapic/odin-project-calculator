b60bdbf0ea671530d7400dd7c2d75e2c39e3da16
Natan Mustapić <109279227+NatanMustapic@users.noreply.github.com>
2023-02-08 15:50:27 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/* Constants */
+const inputDisplay = document.getElementById("input-display-container")
+const calculationDisplay = document.getElementById("calculation-display-container")
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+
+console.table(numberButtons, operatorButtons)
+
+let firstNumber 
+let secondNumber
+let currentOperation  
+
+/* Events */
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.id))
+)
+
+
+/* Functions */
+
+function appendNumber(number) {
+    inputDisplay.textContent += number;
+}
+
+function setOperation(operation) {
+    if (currentOperation !== null) evaluate()
+    firstOperand = inputDisplay.textContent
+    currentOperation = operation
+    calculationDisplay.textContent = `${firstOperand} ${currentOperation}`
+
+    console.table(firstNumber, secondNumber, currentOperation )
+}
